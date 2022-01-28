17255b738ea2a4e2cc91b840d0da7a545c9a8825
imReset <fffffreinds101@gmail.com>
2022-01-27 16:20:41 -0800

    basic layout for site

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components'
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components'
+import './App.css'
 
 const App = () => {
   return (
@@ -11,10 +12,38 @@ const App = () => {
           <Navbar />
       </div>
         <div className="main">
-
-      </div>
+          <Layout>
+            <div className="routes">
+              <Switch>
+                <Route exact path="/">
+                    <Homepage />
+                </Route>
+                <Route exact path="/exchanges">
+                    <Exchanges />
+                </Route>
+                <Route exact path="/cryptocurrencies">
+                    <Cryptocurrencies />
+                </Route>
+                <Route exact path="/crypto/coin:Id">
+                    <CryptoDetails />
+                </Route>
+                <Route exact path="/news">
+                    <News />
+                </Route>
+              </Switch>
+            </div>
+          </Layout>
       <div className="footer">
-
+          <Typography.Title level={5} style={{ color: "white", textAlign: "center"}}>
+            Crypto App <br />
+            All Rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/Exchanges">Exchanges</Link>
+            <Link to="/News">News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   );
